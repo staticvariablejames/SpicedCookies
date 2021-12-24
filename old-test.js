97568a03f80cebcb0524e64c587ae9d7efd0a8d9
Static Variable James <staticvariablejames@gmail.com>
2021-12-24 15:30:13 -0600

    Remove old-test.ts.

--- old-test.js
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// See https://github.com/staticvariablejames/CCtest
-
-function testImplicitAssumptions() {
-    console.assert(!Game.prefs.popup); // Used to simplify message in Spice.init
-}
-testImplicitAssumptions();
